5171f8313904df547f503694b05505b646958027
Omar Flores <52006093+Cj72001@users.noreply.github.com>
2020-04-19 01:29:31 -0600

    "CalculadoraImpuestos" methods modified

--- Parcial1/src/com/ORFA/x00038619/CalculadoraImpuestos.java
+++ Parcial1/src/com/ORFA/x00038619/CalculadoraImpuestos.java
@@ -3,17 +3,29 @@ package com.ORFA.x00038619;
 //Clase estatica
 public final class CalculadoraImpuestos
 {
-    //Atributo
+    //Atributo estaticos.
     private static double totalRenta, totalISSS, totalAFP;
 
     private CalculadoraImpuestos(){}
 
     //Sugerencia: utilizar 'instanceof' en el metodo calcularPago
     //Ya que depende del tipo de Empleado
-    //Metodo que retornara el salario menos renta, ISSS, AFP. Dependiendo del tipo de emplead
+
+    //Metodo que retornara el salario menos renta, ISSS, AFP. Dependiendo del tipo de empleado
     public static double calcularPago  (Empleado empleado) //Variable de tipo objeto
     {
+        //Servicio Profesional
+        totalRenta = empleado.salario * 0.1;
+        empleado.setSalario(empleado.salario - totalRenta);
+        return empleado.getSalario();
 
+        //PlazaFija
+        double Resatante = 0;
+        totalAFP = empleado.salario * 0.0625;
+        totalISSS = empleado.salario * 0.03;
+        Resatante = (empleado.salario - totalAFP - totalISSS);
+
+        //Falta calcular renta...
     }
 
     public static void mostrarTotales()//mostrar totales de AFP, ISSS, RENTA
